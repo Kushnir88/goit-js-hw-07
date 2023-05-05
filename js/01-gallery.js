98f96b220d249ca9eb64e44b9ffba6bc575b26ab
Igor Kushnir <mr.kushnir.igor@gmail.com>
2023-05-05 16:58:03 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,57 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї//
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryListItem = galleryItems
+    .map(
+        (item) =>
+            `<li class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                    />
+                </a>
+            </li>`
+    )
+    .join("");
+
+galleryList.insertAdjacentHTML('beforeend', galleryListItem);
+
+// Реалізація делегування на ul.gallery і отримання url великого зображення.
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+galleryList.addEventListener('click', openOriginalImage);
+let modalImage;
+
+
+function openOriginalImage(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+
+    document.addEventListener('keydown', closeModalonEsc);
+
+    const originalImage = event.target.dataset.source;
+
+    modalImage = basicLightbox.create(`
+		<img width="1400" height="900" src="${originalImage}">
+	`);
+    modalImage.show();
+};
+
+//==============close on escape=================
+
+    function closeModalonEsc(e) {
+        e.preventDefault();
+        if (e.key === 'Escape') {
+            modalImage.close();
+            document.removeEventListener('keydown', closeModalonEsc);
+        }
+    }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryNextList = document.querySelector(".gallery");
+
+const galleryNextItem = galleryItems
+    .map(
+        (item) =>
+            `<li class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>
+            </li>`
+    )
+    .join("");
+
+galleryNextList.insertAdjacentHTML('beforeend', galleryNextItem);
+
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250',
+    captionPosition: 'bottom'
+});
